db140dea7518e81f1a6a66cf5cb27ebcc8040fa9
IvAn711737 <a.abalian2012@yandex.ru>
2022-07-29 08:19:08 +0000

    Add: добавленны модель управления дроном

---
+++ src/puzzle_motion/controll/scripts/motion_server_in_doors.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+import rospy
+
+# from controll.srv import Takeoff, Response
+from controll.srv import Takeoff1, Takeoff1Response
+from takeoff_common.takeoffpy import MavController, AutoPilot
+
+
+class TakeoffHandler:
+    """Класс для обработки запросов к сервису takeoff"""
+
+    def __init__(self):
+        ap_type = rospy.get_param("~ap_type", AutoPilot.PX4)
+        use_vision_odometry = rospy.get_param("~use_vision_odometry", False)
+        self.drone = MavController.create_controller(ap_type, use_vision_odometry=use_vision_odometry)
+        self.is_takeoff=False
+        self.safe_zone=3 # Размер стороны квадрата в метрах за приделы которого дрон не должен вылетать ВАЖНО!нуливая точка (точка взлёта находится в центре этого квадрата)
+        self.safe_max_height=1.5 # Ограничение максимальной высоты подёма в метрах
+        self.safe_min_height=0.2 # Ограничивает минмальную допустимую высоту в метрах
+    def handle_takeoff(self, req):
+        """Обрабатывает запрос к сервису takeoff_landing
+
+        Args:
+            req (Takeoff): запрос к сервису, содержащий высоту и требование посадки
+
+        Returns:
+            TakeoffResponse: True - удачно взлет/приземлился, False - нет
+        """
+        print(req)
+        try:
+            if req.land:
+                self.drone.land()
+                self.is_takeoff=False
+            elif abs(req.x)>self.safe_zone/2 or abs(req.y)>self.safe_zone/2 or req.z>self.safe_max_height or req.z<self.safe_min_height:
+                print ("выход за приделы безопастной зоны")
+                # self.drone.hover_timer_cb()
+                # self.drone.land()
+            elif self.is_takeoff==False:
+                self.is_takeoff=True
+                self.drone.takeoff(req.z)
+                self.drone.go2point(x=req.x, y=req.y, z=req.z, tolerance=0.1)
+                
+            else:
+                 self.drone.go2point(x=req.x, y=req.y, z=req.z, tolerance=0.1)
+            return Takeoff1Response(True)
+        except:
+            return Takeoff1Response(False)
+        
+        
+def takeoff_landing_server():
+    """Запускает сервер сервиса takeoff_landing """
+    rospy.init_node('takeoff_server')
+    th = TakeoffHandler()
+    takeoff_srv = rospy.Service('takeoff_landing', Takeoff1, th.handle_takeoff)
+    print("Ready to takeoff or land")
+    rospy.spin()
+
+
+if __name__ == "__main__":
+    takeoff_landing_server()
