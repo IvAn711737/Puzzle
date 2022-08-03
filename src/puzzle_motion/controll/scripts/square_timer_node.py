ec44e1a265c639fefdc7da99e16f21edacbeac8b
IvAn711737 <a.abalian2012@yandex.ru>
2022-08-03 09:24:31 +0000

    Fix: исправлены пррбелы

--- src/puzzle_motion/controll/scripts/square_timer_node.py
+++ src/puzzle_motion/controll/scripts/square_timer_node.py
@@ -8,8 +8,8 @@ from takeoff_common.takeoffpy import MavController, AutoPilot
 from re import X
 import numpy as np
 takeof_flag=0
-
 poses=[[0, 0], [0.5, 0.5], [-0.5, 0.5], [-0.5, -0.5], [0.5, -0.5], [0, 0]]
+
 if __name__ == "__main__":
     rospy.init_node("position_hold_test", anonymous=True)
     # Считываем параметры из launch-файла
@@ -23,11 +23,12 @@ if __name__ == "__main__":
     for i in range(len(poses)):
         try:
             tic = time.perf_counter()
-            drone.go2point(poses[i] [0], poses[i] [1], altitude, tolerance=0.1)
+            drone.go2point(poses[i][0], poses[i][1], altitude, tolerance=0.1)
             toc = time.perf_counter()
             if i>0:
-                rospy.loginfo(f"Перемещение из точки {poses[i-1] [0], poses[i-1] [1] } в точку {poses[i] [0], poses[i] [1]}  заняло {toc - tic:0.4f} секунд")
+                rospy.loginfo(f"Перемещение из точки {poses[i-1][0], poses[i-1][1] } в точку {poses[i][0], poses[i][1]}  заняло {toc - tic:0.4f} секунд")
         except Exception as e:
             rospy.logerr(e)
             drone.land()
+            break
     drone.land()
